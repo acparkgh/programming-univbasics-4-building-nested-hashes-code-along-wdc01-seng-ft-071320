e01de6f928e49da1ace4bb3aa51b1b8f8df7c252
Andrew Park <acpark123@gmail.com>
2020-06-08 03:05:32 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -34,7 +34,7 @@ def third_challenge
         #your key/value pairs here
         :name => "Juliet",
         :age => "15",
-        :statua => "alive"
+        :status => "alive"
         },
       :heroine_friends => []
    }
